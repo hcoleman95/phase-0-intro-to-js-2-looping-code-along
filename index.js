561868861ec4002146b709b892204ed2ceb51aa9
Howard Coleman <howard.coleman16@gmail.com>
2023-08-16 17:30:47 -0400

    completed looping code along

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+const names  = ["charlie", "Samip", "Ali"]
+
+
+function writeCards(array, event){
+    const greetings = []
+  for(let i = 0; i < array.length; i++){
+    greetings.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`)
+  }
+ return greetings;
+}
+
+
+function countDown(num){
+    while(num >= 0){
+        console.log(num)
+        num--
+    }
+}
